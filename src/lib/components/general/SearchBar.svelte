b9cdfb07406cd89407fecf6fc88c291e887f73cb
Sand7 <geometrygaming8@gmail.com>
2024-08-21 16:12:35 +0800

    search bar component

---
+++ src/lib/components/general/SearchBar.svelte
@@ -0,0 +1,16 @@
+<script>
+    import { Input } from '$lib/components/ui/input'
+    import { Search } from 'lucide-svelte'
+</script>
+
+<div class="w-full relative flex justify-center">
+    <label for="search-bar">
+        <Search class="relative top-2 left-8 w-[20px]" />
+    </label>
+    <Input
+        id="search-bar"
+        type="text"
+        placeholder="Search items..."
+        class="max-w-xs pl-10 border-2 border-gray-200"
+    />
+</div>
