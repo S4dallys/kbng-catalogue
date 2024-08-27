4cfe034c7d10ef618d8ca2eaa2a8b90477d20757
Sand7 <geometrygaming8@gmail.com>
2024-08-27 12:14:32 +0800

    imported select component

---
+++ src/lib/components/general/Select.svelte
@@ -0,0 +1,17 @@
+<script>
+    import * as Select from '$lib/components/ui/select'
+
+    export let content
+    export let selected
+</script>
+
+<Select.Root bind:selected>
+    <Select.Trigger class="w-[180px]">
+        <Select.Value placeholder="Theme" />
+    </Select.Trigger>
+    <Select.Content>
+        {#each content as item}
+            <Select.Item {...item} />
+        {/each}
+    </Select.Content>
+</Select.Root>
